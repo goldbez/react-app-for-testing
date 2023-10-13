98c696001fe794de74085d8f668be6cc313b509b
Zachary Goldberg <goldbez@amazon.com>
2023-10-13 16:04:42 -0700

    noop trigger job

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          my text 1
+          abc
         </p>
         <a
           className="App-link"
