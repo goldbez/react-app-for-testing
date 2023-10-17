e2ac677766738a3f6c2c932626e4d9d00670b671
Zachary Goldberg <goldbez@amazon.com>
2023-10-17 09:47:29 -0700

    chore: noop trigger build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-2zach
+3zach
         </p>
         <a
           className="App-link"
